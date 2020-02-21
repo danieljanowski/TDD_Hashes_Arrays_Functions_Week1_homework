c4b68ef8622b0c478ca65296d909106334cdbfcd
danieljanowski <daniel.janowski@gmail.com>
2020-02-21 21:44:05 +0000

    Finished Optional

--- pet_shop.rb
+++ pet_shop.rb
@@ -65,7 +65,7 @@ def customer_can_afford_pet(customer, new_pet)
 end
 
 def sell_pet_to_customer (pet_shop, pet, customer)
-  if customer_can_afford_pet(customer, pet)
+  if pet != nil && customer_can_afford_pet(customer, pet)
     add_pet_to_customer(customer, pet)
     increase_pets_sold(pet_shop, 1)
     remove_customer_cash(customer, pet[:price])
