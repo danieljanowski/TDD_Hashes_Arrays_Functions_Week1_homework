c09db1bbfa24c864e34872ac23f8efd90adb13e4
danieljanowski <daniel.janowski@gmail.com>
2020-02-21 17:08:08 +0000

    second commit

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,3 +1,35 @@
+require 'pry-byebug'
+
 def pet_shop_name(pet_shop)
   return pet_shop[:name]
+end
+
+def total_cash(pet_shop)
+  return pet_shop[:admin][:total_cash]
+end
+
+def add_or_remove_cash(pet_shop, amount)
+  pet_shop[:admin][:total_cash] += amount
+end
+
+def pets_sold(pet_shop)
+  return pet_shop[:admin][:pets_sold]
+end
+
+def increase_pets_sold(pet_shop, amount)
+  pet_shop[:admin][:pets_sold] += amount
+end
+
+def stock_count(pet_shop)
+  return pet_shop[:pets].length
+end
+
+def pets_by_breed(pet_shop, breed)
+  breed_number = 0
+  # p pet_shop[:pets][:breed]
+  return pet_shop[:pets].select{|pet| pet[:breed] == breed}
+
+  # for pet in pet_shop[:pets]
+  #
+  # end
 end
